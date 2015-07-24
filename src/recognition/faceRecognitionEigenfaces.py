993fe1a11dcd86b66aa1adadfc356fd388b1c0ec
Roland Dudko <roland.dudko@gmail.com>
2015-07-24 14:15:50 +0200

    Add face recognition functionality

--- src/recognition/faceRecognitionEigenfaces.py
+++ src/recognition/faceRecognitionEigenfaces.py
@@ -1,73 +1,73 @@
 import cv2
-import numpy
+import numpy as np
 import random
-import math
+import os
 
 import utilities as util
 from util.event_logger import EventLogger, ConsoleLogger
+import faceDetectionWebcam as fdw
 
-def create_and_train_model_from_dict(label_matrix):
-    """ Create eigenface model from dict of labels and images """
-    model = cv2.createEigenFaceRecognizer()
-    model.train(label_matrix.values(), numpy.array(label_matrix.keys()))
-    return model
+EventLogger.add_logger(ConsoleLogger("ConsoleLogger", EventLogger.EVENT_LOG_LEVEL))
 
-def predict_image_from_model(model, image):
-    """ Given an eigenface model, predict the label of an image"""
-    return model.predict(image)
+class faceRecognitionEigenfaces:
+    def __init__(self):
+        pass
 
-def read_csv(filename='faces.csv'):
-    EventLogger.info(filename)
+    def create_and_train_model_from_dict(self, label_matrix):
+        """ Create eigenface model from dict of labels and images """
+        model = cv2.createEigenFaceRecognizer()
+        model.train(label_matrix.values(), np.array(label_matrix.keys()))
+        return model
 
-    """ Read a csv file """
-    csv = open(filename, 'r')
-    return csv
+    def read_csv(self, filename):
+        EventLogger.info(filename)
 
-def prepare_training_testing_data(file):
-    """ prepare testing and training data from file"""
-    lines = file.readlines()
-    training_data, testing_data = split_test_training_data(lines)
-    return training_data, testing_data
+        """ Read a csv file """
+        csv = open(filename, 'r')
+        return csv
 
-def create_label_matrix_dict(input_file):
-    """ Create dict of label -> matricies from file """
-    ### for every line, if key exists, insert into dict, else append
-    label_dict = {}
+    def create_label_matrix_dict(self, input_file):
+        """ Create dict of label -> matricies from file """
+        ### for every line, if key exists, insert into dict, else append
+        label_dict = {}
 
-    for line in input_file:
-        ## split on the ';' in the csv separating filename;label
-        filename, label = line.strip().split(';')
+        for line in input_file:
+            ## split on the ';' in the csv separating filename;label
+            filename, label = line.strip().split(';')
 
-        ##update the current key if it exists, else append to it
-        if label_dict.has_key(int(label)):
-            current_files = label_dict.get(label)
-            numpy.append(current_files,read_matrix_from_file(filename))
-        else:
-            label_dict[int(label)] = read_matrix_from_file(filename)
+            ##update the current key if it exists, else append to it
+            if label_dict.has_key(int(label)):
+                current_files = label_dict.get(label)
+                np.append(current_files, self.read_matrix_from_file(filename))
+            else:
+                label_dict[int(label)] = self.read_matrix_from_file(filename)
 
-    return label_dict
+        return label_dict
 
-def split_test_training_data(data, ratio=0.2):
-    """ Split a list of image files by ratio of training:test data """
-    test_size = int(math.floor(ratio*len(data)))
-    random.shuffle(data)
-    return data[test_size:], data[:test_size]
+    def read_matrix_from_file(self, filename):
+        """ read in grayscale version of image from file """
+        return cv2.imread(filename, cv2.CV_LOAD_IMAGE_GRAYSCALE)
 
-def read_matrix_from_file(filename):
-    """ read in grayscale version of image from file """
-    return cv2.imread(filename, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+    def _found_face(self,frame, faces):
+        training_data = self.read_csv(("./recognition/faceDatabase/faces.csv")).readlines()
+        data_dict = self.create_label_matrix_dict(training_data)
+        model = self.create_and_train_model_from_dict(data_dict)
 
-if __name__ == "__main__":
-    EventLogger.add_logger(ConsoleLogger("ConsoleLogger", EventLogger.EVENT_LOG_LEVEL))
+        for img in faces:
+            rnd = random.randint(0, 10000000)
+            path_file = "./recognition/tmp/" + str(rnd) + ".pgm"
+            util.save_image(path_file,img)
 
-    csvPath = "./faceDatabase"
-    util.create_csv(csvPath)
+            predicted_label = model.predict(self.read_matrix_from_file(path_file))
+            for i in predicted_label:
+                #TODO: return index to Marvs module
+                print 'Predicted: %(predicted)s ' % {"predicted": i}
+            try:
+                os.remove(path_file)
+            except Exception as e:
+                EventLogger.error(e)
 
-    training_data, testing_data = prepare_training_testing_data(read_csv((csvPath + "/faces.csv")))
-    data_dict = create_label_matrix_dict(training_data)
-    model = create_and_train_model_from_dict(data_dict)
-
-    for line in testing_data:
-        filename, label =  line.strip().split(';')
-        predicted_label = predict_image_from_model(model, read_matrix_from_file(filename))
-        print 'Predicted: %(predicted)s  Actual: %(actual)s' %  {"predicted": predicted_label[0], "actual": label}
+    def start_process(self):
+        csvPath = "./recognition/faceDatabase"
+        util.create_csv(csvPath)
+        fdw.face_detection_webcam(self._found_face)
