fadac08615f30219fbb0483ae3a4fd9038f67778
Marv <marvin.lutz@fh-bielefeld.de>
2015-07-28 12:24:04 +0200

    added dual button support

--- src/main_fr.py
+++ src/main_fr.py
@@ -8,14 +8,53 @@ import recognition.generateFaceDatabase as gfd
 import recognition.faceRecognitionEigenfaces as fre
 import recognition.utilities as util
 from util.event_logger import EventLogger, ConsoleLogger
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.bricklet_dual_button import DualButton
 
+HOST = "localhost"
+PORT = 4223
+DB_UID = "mAo"
+db = None
+
+# control values
+face_recognition_state = False
+face_recognition_running = True
+button_right_state = False
 process = False
+
+
+def __cb_state_changed(button_l, button_r, led_r, led_l):
+    global face_recognition_state, face_recognition_running, process
+
+    if button_r == DualButton.BUTTON_STATE_PRESSED:
+        led_r = DualButton.LED_STATE_OFF
+        db.set_selected_led_state(DualButton.LED_RIGHT, led_r)
+        # exit program
+
+        face_recognition_running = False
+
+    if not process:
+        if button_l == DualButton.BUTTON_STATE_PRESSED:
+            led_l = DualButton.LED_STATE_ON
+            db.set_selected_led_state(DualButton.LED_LEFT, led_l)
+            face_recognition_state = True
+        else:
+            led_l = DualButton.LED_STATE_OFF
+            db.set_selected_led_state(DualButton.LED_LEFT, led_l)
+            face_recognition_state = False
+
+            #print "face_state = "+str(face_recognition_state)+"\nface_running: " + str(face_recognition_running) + "\nprocess: "+str(process)+"\n---"
+
+
+
 lock = allocate_lock()
 
 def look_for_face():
     face_detection_webcam(found_face)
 
 def found_face(frame, faces):
+    global face_recognition_state, process
+
     if frame is None or faces is None:
         print "found_face: one param is None"
 
@@ -28,18 +67,37 @@ def found_face(frame, faces):
         # 'cv2.cv.cvmat'
         util.save_image("./image_" + str(i) + "_" + str(rnd) + ".pgm", val)
         #util.save_image("./frame_" + str(rnd) + ".jpeg", (cv2.cv.fromarray(frame)))
-        util.show_image("Face", val )
+        #util.show_image("Face", val )
 
 
     # FIXME: same face as last frame
     # Eigenfaces/Fishfaces
 
-    global process
+    # global process
+    print "finished"
+    db.set_selected_led_state(DualButton.LED_LEFT, DualButton.LED_STATE_OFF)
+    face_recognition_state = False
     process = False
 
 
 if __name__ == '__main__':
+
     EventLogger.add_logger(ConsoleLogger("ConsoleLogger", EventLogger.EVENT_LOG_LEVEL))
+
+    ipcon = IPConnection()
+    db = DualButton(DB_UID, ipcon)
+
+    try:
+        ipcon.connect(HOST, PORT)
+    except Exception as e:
+        EventLogger.critical("A critical error occur: " + str(e))
+        sys.exit(1)
+
+    # init values
+    db.set_selected_led_state(DualButton.LED_LEFT, DualButton.LED_STATE_OFF)
+    db.set_selected_led_state(DualButton.LED_RIGHT, DualButton.LED_STATE_ON)
+    db.register_callback(db.CALLBACK_STATE_CHANGED, __cb_state_changed)
+
 
     if len(sys.argv) != 2:
         EventLogger.error("Please start this Program with the parameter 1 or 0")
@@ -58,15 +116,25 @@ if __name__ == '__main__':
         EventLogger.info("Start Mode default: Detecting Faces")
         try:
             process = False
-            while True:
-                if not process:
+            print "face_state = " + str(face_recognition_state) + "\nface_running: " + str(
+                face_recognition_running) + "\nprocess: " + str(process) + "\n---"
+            while face_recognition_running:
+                if not process and face_recognition_state:
                     lock.acquire()
                     process = True
                     lock.release()
 
-                    process = start_new_thread(face_detection_webcam, (found_face,))
+                    print "STARTED"
+                    start_new_thread(face_detection_webcam, (found_face,))
+
+            raise KeyboardInterrupt
 
         except KeyboardInterrupt:
             EventLogger.info("Will now end this program")
         finally:
             end_webcam()
+
+    db.set_selected_led_state(DualButton.LED_LEFT, DualButton.LED_STATE_OFF)
+    db.set_selected_led_state(DualButton.LED_RIGHT, DualButton.LED_STATE_OFF)
+
+    ipcon.disconnect()
