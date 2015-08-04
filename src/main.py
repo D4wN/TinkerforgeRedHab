c6094d56386fc2b5adda76db60aff4771fc64857
Marvin Lutz <marvin.lutz@fh-bielefeld.de>
2015-08-04 13:13:27 +0200

    added argv to main menu
    0 => nfc/rfid write
    1 => nfc/rfid recognition
    2 => Face Recognition

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ from recognition.recognizer import FaceRecognizer, NfcRfidRecognizer
 
 # print sys.path
 
-from profiling.profiler import Profiler
 from util.event_logger import EventLogger, ConsoleLogger
 
 
@@ -13,17 +12,36 @@ def __init_event_logging():
     #EventLogger.add_logger(FileLogger("FileLogger", EventLogger.EVENT_LOG_LEVEL, EventLogger.EVENT_FILE_LOGGING_PATH))
 
 
-
 if __name__ == "__main__":
-    runner = RecognitionRunner(NfcRfidRecognizer())
-    runner.start()
+    __init_event_logging()
+    EventLogger.debug(
+        "\nargv[1] == 0 => NFC/RFID Writing argv[2] = name\nargv[1] == 1 => NFC/RFID Recognition\nargv[1] == 2 => Face Recognition")
 
-"""
-    name = "Marvin Lutz"
-    print name
-    arr = Utils.string_to_byte_array(name)
-    print str(arr)
-    new_name = Utils.byte_array_to_string(arr)
-    print new_name
+    if len(sys.argv) < 2:
+        EventLogger.error("Aborting Program, wrong Parameters!")
 
-    nfc()"""
+    elif sys.argv[1] == "0":
+        EventLogger.debug("argv[1] == 0")
+        EventLogger.info("NFC/RFID Writer")
+        runner = RecognitionRunner(NfcRfidRecognizer())
+        if len(sys.argv) >= 3:
+            name = str(sys.argv[2])
+            runner.start([name])
+        else:
+            EventLogger.debug("argv[2] == ??? Missing name! Aborting Program")
+
+    elif sys.argv[1] == "1":
+        EventLogger.debug("len(argv) == 1")
+        EventLogger.info("NFC/RFID Recognition")
+        runner = RecognitionRunner(NfcRfidRecognizer())
+        runner.start()
+
+    elif sys.argv[1] == "2":
+        EventLogger.debug("argv[1] == 2")
+        EventLogger.info("Face Recognition")
+        runner = RecognitionRunner(FaceRecognizer())
+        runner.start([None, 2])
+
+    else:
+        EventLogger.debug("argv[1] == " + str(sys.argv[1]))
+        EventLogger.error("Aborting Program, wrong Parameters!")
