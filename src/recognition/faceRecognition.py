2a9dcfba3ef9aff5beac9963d3eeb737886da6e6
Roland Dudko <roland.dudko@gmail.com>
2015-08-04 11:42:46 +0200

    Add eigenfaces, fisherfaces, lbph - new model creation method

--- src/recognition/faceRecognition.py
+++ src/recognition/faceRecognition.py
@@ -18,25 +18,42 @@ class faceRecognition:
         # select mode for the face recognition
 
         self.model_eigenfaces = cv2.createEigenFaceRecognizer()
-        #self.model_fisherfaces = cv2.createFisherFaceRecognizer()
-        #self.model_lbph = cv2.createLBPHFaceRecognizer()
+        self.model_fisherfaces = cv2.createFisherFaceRecognizer()
+        self.model_lbph = cv2.createLBPHFaceRecognizer()
 
         sep = os.sep
         self.csvPath = ".%srecognition%sfaceDatabase%s" % (sep,sep,sep)
 
+    def update_model(self):
+        training_data = self.read_csv((self.csvPath + "faces.csv")).readlines()
 
-    def create_and_train_model_from_dict(self, label_matrix):
-        """ Create eigenface model from dict of labels and images """
-        #FIXME: If there is a model and nothing changed -> load the model
-        # Else:
-        self.model_eigenfaces.train(label_matrix.values(), np.array(label_matrix.keys()))
+        data_dict = self.create_label_matrix_dict(training_data)
 
-        '''
+        self.model_eigenfaces.train(data_dict.values(), np.array(data_dict.keys()))
+        self.model_fisherfaces.train(data_dict.values(), np.array(data_dict.keys()))
+        self.model_lbph.train(data_dict.values(), np.array(data_dict.keys()))
+
         try:
-           self.model.save("./recognition/faceDatabase/eigenface-model.xml")
+            self.model_eigenfaces.save(self.csvPath + "eigenface-model.xml")
+            self.model_fisherfaces.save(self.csvPath + "fisherfaces-model.xml")
+            self.model_lbph.save(self.csvPath + "lbph-model.xml")
+
         except Exception as e:
             EventLogger.error(e)
-        '''
+
+    def load_model(self):
+        try:
+            if os.path.isfile(self.csvPath + "eigenface-model.xml"):
+                self.model_eigenfaces.load(self.csvPath + "eigenface-model.xml")
+                self.model_fisherfaces.load(self.csvPath + "fisherfaces-model.xml")
+                self.model_lbph.load(self.csvPath + "lbph-model.xml")
+
+            else:
+                raise Exception("No model was found. Will create a new one")
+
+        except Exception as e:
+            EventLogger.error(e)
+            self.update_model()
 
     def read_csv(self, filename):
         EventLogger.info(filename)
@@ -66,20 +83,14 @@ class faceRecognition:
         return cv2.imread(filename, cv2.CV_LOAD_IMAGE_GRAYSCALE)
 
     def _recognize_face(self,frame, faces):
-        training_data = self.read_csv(("./recognition/faceDatabase/faces.csv")).readlines()
-        data_dict = self.create_label_matrix_dict(training_data)
-        if len(data_dict) == 0:
-            EventLogger.error("There is no face database available")
-            return
-
-        self.create_and_train_model_from_dict(data_dict)
-        cv2.namedWindow("webcam_face")
+        self.load_model()
 
         for img in faces:
             rnd = random.randint(0, 10000000)
             path_file = "./recognition/tmp/" + str(rnd) + ".pgm"
             util.save_image(path_file, img)
 
+            # fix the size of the input image If needed
             sized_face = None
             input_image = self.read_matrix_from_file(path_file)
             width, height = cv2.cv.GetSize(cv2.cv.fromarray(input_image))
@@ -90,8 +101,15 @@ class faceRecognition:
             else:
                 sized_face = input_image
 
+            # actual face recognition
             predicted_label_eigenfaces, conf_eigenfaces = self.model_eigenfaces.predict(sized_face)
             EventLogger.info('Eigenfaces: Predicted: %(predicted)s Confidence : %(confidence )s ' % {"predicted": predicted_label_eigenfaces, "confidence ":conf_eigenfaces})
+
+            predicted_label_fisherfaces, conf_fisherfaces = self.model_fisherfaces.predict(sized_face)
+            EventLogger.info('Fisherfaces: Predicted: %(predicted)s Confidence : %(confidence )s ' % {"predicted": predicted_label_fisherfaces, "confidence ":conf_fisherfaces})
+
+            predicted_label_lbph, conf_lbph = self.model_lbph.predict(sized_face)
+            EventLogger.info('LBPH: Predicted: %(predicted)s Confidence : %(confidence )s ' % {"predicted": predicted_label_lbph, "confidence ":conf_lbph})
 
             try:
                 os.remove(path_file)
